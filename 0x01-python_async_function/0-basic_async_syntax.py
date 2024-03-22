7ca58a08493c336144d3c3b1144f6deaa22aaac2
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-03-22 22:34:55 +0000

    simple async3

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-import asyncio
-import random
 """
 importing necessary libraries
 Author: Nafeesah
 """
+import asyncio
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
