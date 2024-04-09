bea02260ab569419fbb99244ec47ab234ec86a66
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-09 01:03:32 +0100

    access nestedcv map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,3 +16,14 @@ class TestAccessNestedMap(unittest.TestCase):
         """Test that the method returns what it is supposed to"""
         res = access_nested_map(nested_map, path)
         self.assertEqual(res, expected_value)
+
+    @parameterized.expand([
+        ({}, ("a",), "Key 'a' not found"),
+        ({"a": 1}, ("a", "b"), "Key 'b' not found in {'a': 1}")
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected_message):
+        """Test that the method raises KeyError with correct message"""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        
+        self.assertEqual(str(context.exception), expected_message)
